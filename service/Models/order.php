dc8fd68a1dfea8a3664f944de260e27e55c6866a
Tam Le <leminhtamkhtn@gmail.com>
2017-01-05 01:09:09 +0700

    cap nhat order va product model

--- service/Models/order.php
+++ service/Models/order.php
@@ -36,8 +36,47 @@ class Order{
             return $e->getMessage();
         }
     }
+    /* data = object(
+     *      user_id
+     *      total_money
+     *      name
+     *      address
+     *      phone
+     *      email
+     *      order_detail= array(
+     *          0=> product_detail_id
+     *
+     *      )
+     */
     function  addOrder($data){
+        $created_day    = new Date('Y-m-d H:i:s');
+        $user_id        = $data['user_id'];
+        $total_money    = $data['total_money'];
+        $name           = $data['name'];
+        $address        = $data['address'];
+        $phone          = $data['phone'];
+        $email          = $data['email'];
+        $sqlOrder = "INSERT INTO orders (created_day,total_money,user_id,name,address,phone,email)". "VALUES ('$created_day','$user_id','$total_money','$name','$address','$phone','$email')";
+        $temp = $this->con->prepare($sqlOrder);
+        $temp->execute();
 
+        $sqlSelect = "SELECT order_id FROM orders WHERE created_day=:created_day AND user_id=:user_id";
+        $temp1=$this->con->prepare($sqlSelect);
+        $temp1->bindParam('created_day',$created_day);
+        $temp1->bindParam('user_id',$user_id);
+        $temp1->execute();
+
+        //lấy product_id từ temp1;
+        $list = $temp1->fetchAll(PDO::FETCH_BOTH);
+        $orderId = $list['order_id'];
+
+        $listOrderDetail = $data['order_detail'];
+        foreach ($listOrderDetail as $item){
+            $product_detail_id = $item['product_detail_id'];
+            $sqlProductDetail = "INSERT INTO order_detail (product_detail_id)"."VALUES ('$product_detail_id')";
+            $temp2 = $this->con->prepare($sqlProductDetail);
+            $temp2->execute();
+        }
     }
 
     function deleteOrder($order_id){
--- service/Models/product.php
+++ service/Models/product.php
@@ -37,8 +37,52 @@ class Product{
             return $e->getMessage();
         }
     }
+    /* data = object(
+     *       name
+     *       description
+     *       type
+     *       price
+     *       image_name
+     *       count
+     *       product = array(
+     *          0 => { size, color,count}
+     *          1 => { size, color,count}
+     *
+     *          )
+     *      )
+     */
     function  addProduct($data){
-        
+        $name = $data['name'];
+        $description = $data['description'];
+        $type = $data['type'];
+        $price = $data['price'];
+        $image_name = $data['image_name'];
+        $count = $data['count'];
+        $created_day = new Date('Y-m-d H:i:s');
+        $sqlProduct = "INSERT INTO products (product_name,description,type,price,created_day,image_name,count)". "VALUES ( '$name','$description','$type','$price','$created_day','$image_name','$count')";
+        $temp=$this->con->prepare($sqlProduct);
+        $temp->execute();
+
+        $sqlSelect = "SELECT product_id FROM products WHERE created_day=:created_day AND product_name=:name";
+        $temp1=$this->con->prepare($sqlSelect);
+        $temp1->bindParam('created_day',$created_day);
+        $temp1->bindParam('name',$name);
+        $temp1->execute();
+
+        //lấy product_id từ temp1;
+        $list = $temp1->fetchAll(PDO::FETCH_BOTH);
+        $productId = $list['product_id'];
+
+        $listProductDetail = $data['product'];
+        foreach ($listProductDetail as $item){
+            $size = $item['size'];
+            $color = $item['color'];
+            $countDetail = $item['count'];
+            $sqlProductDetail = "INSERT INTO product_detail (product_id,size,color,count)"."VALUES ('$productId','$size','$color','$countDetail')";
+            $temp2 = $this->con->prepare($sqlProductDetail);
+            $temp2->execute();
+        }
+
     }
 
     function deleteProduct($data){
